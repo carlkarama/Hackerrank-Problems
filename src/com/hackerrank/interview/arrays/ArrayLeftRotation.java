a703f741ab56c94bd1a7cebcefbecc0edaa6f48b
Carl Karama <carlkarama4@gmail.com>
2024-02-19 23:52:54 +1100

    feat: add base code

---
+++ src/com/hackerrank/interview/arrays/ArrayLeftRotation.java
@@ -0,0 +1,13 @@
+package com.hackerrank.interview.arrays;
+
+import java.util.List;
+
+public class ArrayLeftRotation {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> rotLeft(List<Integer> a, int d) {
+        int lengthOfArray = a.size();
+    }
+}
